2bae31c7042d7efa4b028a10ea4b93e9e48d1e40
PRIYANSHUSINGH2003 <priyanshusingh00004@gmail.com>
2025-05-24 14:08:25 +0530

    fix: footer

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -7,12 +7,14 @@ import { usePathname } from "next/navigation";
 const PublicLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
   const pathname = usePathname();
   const isDoctorHome = pathname.startsWith("/doctor/home");
+  const isDoctorManagement_Patient = pathname.startsWith("/doctor/management/patient");
 
   return (
     <div className="min-h-screen flex flex-col overflow-x-hidden">
       <Navbar />
       <main className="flex-1">{children}</main>
       {!isDoctorHome && <Footer />}
+      {!isDoctorManagement_Patient && <Footer />}
     </div>
   );
 };
